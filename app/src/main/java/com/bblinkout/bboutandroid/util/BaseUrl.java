e1c08e674fbb471d9153664e404c83f5738fb375
Supreethamg <supreethamg@gmail.com>
2018-04-27 00:27:22 -0700

    added thhis file to have base url

---
+++ app/src/main/java/com/bblinkout/bboutandroid/util/BaseUrl.java
@@ -0,0 +1,6 @@
+package com.bblinkout.bboutandroid.util;
+
+public class BaseUrl {
+
+    public static final String BASE_URL ="http://default-environment.64dhghmhih.us-east-2.elasticbeanstalk.com/api/";
+}
